8f9d0585ddff6b0e447d0e8ff3eb50c186a134bb
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-11-26 06:59:30 -0800

    drafted setContactSelected in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './components/Nav'
 import About from './components/About'
 import Portfolio from './components/Portfolio'
 import ContactForm from './components/Contact'
-import Resume from './component/Resume'
+import Resume from './components/Resume'
 import './App.css'
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false)
+
   return (
     <div>
-      <Nav>
-      </Nav>
+      <Nav
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
         {!contactSelected ? (
           <>
             <About></About>
             <Portfolio></Portfolio>
             <Resume></Resume>
-            </>
+          </>
         ) : (
           <ContactForm></ContactForm>
         )}
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import profilePhoto from '../../assets/images/photo.jpg'
 
-function Nav () {
+function Nav (props) {
+    const {
+        contactSelected,
+        setContactSelected
+    } = props
+
     return (
     <section className="container"> 
         <header className="header row">
@@ -13,11 +18,17 @@ function Nav () {
             <div className="col-8">
                 <nav className="nav">
                     <ul>
-                        <li>About me</li>
-                        <li>Projects</li>
-                        <li>Contact</li>
                         <li>
-                            <a href="https://drive.google.com/file/d/1o-eO67x3Mb3exlK_D9LFarj_NivBnDNv/view?usp=sharing" target="_blank">Resume</a>
+                            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>About me</a>
+                        </li>
+                        <li>
+                            <a data-testid="portfolio" href="#portfolio" onClick={() => setContactSelected(false)}>Projects</a>
+                        </li>
+                        <li className={`${contactSelected && 'navActive'}`}>
+                            <span onClick={() => setContactSelected(true)}>Contact</span>
+                        </li>
+                        <li>
+                            <a data-testid="resume" href="https://drive.google.com/file/d/1o-eO67x3Mb3exlK_D9LFarj_NivBnDNv/view?usp=sharing" target="_blank" onClick={() => setContactSelected(false)}>Resume</a>
                         </li>
                     </ul>
                 </nav>
