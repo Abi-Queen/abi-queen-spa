fd8765d670be862719d2bb7cacf1a488201276cf
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-11-30 14:15:43 -0800

    added gradient, justify, align to card headers

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { FaGithub } from 'react-icons/fa'
-// import quizSS from '../../assets/images/coding-quiz-screenshot.jpg'
-// import regexSS from '../../assets/images/regex-ss.jpg'
+import quizSS from '../../assets/images/coding-quiz-screenshot.jpg'
+import regexSS from '../../assets/images/regex-ss.jpg'
 import empDBSS from '../../assets/images/employeedb-ss.jpg'
 // import shopSS from '../../assets/images/shop-ss.jpg'
-// import teamSS from '../../assets/images/team-ss.jpg'
-// import wildfireSS from '../../assets/images/wildfire-screenshot.jpg'
-// import socialSS from '../../assets/images/social-network-ss.jpg'
+import teamSS from '../../assets/images/team-ss.jpg'
+import wildfireSS from '../../assets/images/wildfire-screenshot.jpg'
+import socialSS from '../../assets/images/social-network-ss.jpg'
 
 function Portfolio () { 
     return (
@@ -15,11 +15,11 @@ function Portfolio () {
                 <div className="col d-flex justify-content-around">
                     <div className="card">
                         <div className="card-header">
-                            <h2 className="card-title">
+                            <h4 className="card-title">
                                 <a href="https://drive.google.com/file/d/1N6tmbxjLtrDPPkPcvb1f79KTqvoa9Rt5/view">Employee Database</a>
                                 <a href="https://github.com/Abi-Queen/Employee-Manager-DB">
                             <FaGithub /></a>
-                            </h2>
+                            </h4>
                             <h5 className="card-text">
                                 MySQL, Node, JavaScript
                             </h5>
@@ -32,17 +32,17 @@ function Portfolio () {
                 <div className="col d-flex justify-content-around">
                     <div className="card">
                         <div className="card-header">
-                            <h2 className="card-title">
+                            <h4 className="card-title">
                                 <a href="https://nverhulp.github.io/wildfire-app/">California Wildfire Readiness App</a>
                                 <a href="https://github.com/nverhulp/wildfire-app">
                                 <FaGithub /></a>
-                            </h2>
+                            </h4>
                             <h5 className="card-text">
                                 HTML, CSS, JavaScript
                             </h5>
                         </div>
                         <div className="card-body">
-                            <img className="card-img" src={empDBSS} alt="screenshot of user input to database"></img>
+                            <img className="card-img" src={wildfireSS} alt="screenshot of user input to database"></img>
                         </div>
                     </div>
                 </div>
@@ -51,11 +51,11 @@ function Portfolio () {
                 <div className="col d-flex justify-content-around">
                     <div className="card">
                         <div className="card-header">
-                            <h2 className="card-title">
+                            <h4 className="card-title">
                                 <a href="https://drive.google.com/file/d/1N6tmbxjLtrDPPkPcvb1f79KTqvoa9Rt5/view">Gist: Email Regex Tutorial</a>
                                 <a href="https://drive.google.com/file/d/1N6tmbxjLtrDPPkPcvb1f79KTqvoa9Rt5/view">
                                 <FaGithub /></a>
-                            </h2>
+                            </h4>
                             <h5 className="card-text">
                                 JavaScript
                             </h5>
@@ -68,11 +68,11 @@ function Portfolio () {
                 <div className="col d-flex justify-content-around">
                     <div className="card">
                         <div className="card-header">
-                            <h2 className="card-title">
+                            <h4 className="card-title">
                                 <a href="https://abi-queen.github.io/Coding-Quiz/">Coding Quiz</a>
                                 <a href="https://github.com/Abi-Queen/Coding-Quiz">
                                 <FaGithub /></a>
-                            </h2>
+                            </h4>
                             <h5 className="card-text">
                                 HTML, CSS, JavaScript
                             </h5>
@@ -87,34 +87,34 @@ function Portfolio () {
                 <div className="col d-flex justify-content-around">
                     <div className="card">
                         <div className="card-header">
-                            <h2 className="card-title">
+                            <h4 className="card-title">
                                 <a href="https://youtu.be/Ru98SC457gA">Social Network API</a>
                                 <a href="https://github.com/Abi-Queen/social-network-API">
                                 <FaGithub /></a>
-                            </h2>
+                            </h4>
                             <h5 className="card-text">
                                 MongoDB, Mongoose, Express, Node
                             </h5>
                         </div>
                         <div className="card-body">
-                            <img className="card-img" src={wildfireSS} alt="screenshot of user input to database"></img>
+                            <img className="card-img" src={socialSS} alt="screenshot of user input to database"></img>
                         </div>
                     </div>
                 </div>
                 <div className="col d-flex justify-content-around">
                     <div className="card">
                         <div className="card-header">
-                            <h2 className="card-title">
+                            <h4 className="card-title">
                                 <a href="https://youtu.be/ut7UswYo3GU">HTML Generator, OOP</a>
                                 <a href="https://github.com/Abi-Queen/engineers">
                             <FaGithub /></a>
-                            </h2>
+                            </h4>
                             <h5 className="card-text">
                                 HTML, CSS, JavaScript, Node, Jest
                             </h5>
                         </div>
                         <div className="card-body">
-                            <img className="card-img" src={empDBSS} alt="screenshot of user input to database"></img>
+                            <img className="card-img" src={teamSS} alt="screenshot of user input to database"></img>
                         </div>
                     </div>
                 </div>
