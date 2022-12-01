44edf3d3728a0ae28e8ab91df36e2bcfb5878c99
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-12-01 06:44:53 -0800

    adjust form borders

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -40,26 +40,28 @@ function ContactForm () {
     return(
         <section className="container" id="contact">
             <h1 data-testid="h1tag">Contact me</h1>
-            <form id="contact-form" onSubmit={handleSubmit}>
-                <div className="row">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-                </div>
-                <div className="row">
-                    <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-                </div>
-                <div className="row">
-                    <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-                </div>
-                {errorMessage && (
-                    <div>
-                        <p className="error-text">{errorMessage}</p>
+            <div className="d-flex">
+                <form id="contact-form" onSubmit={handleSubmit}>
+                    <div className="row">
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
                     </div>
-                )}
-                <button data-testid="button" type="submit">Submit</button>
-            </form>
+                    <div className="row">
+                        <label htmlFor="email">Email address:</label>
+                        <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+                    </div>
+                    <div className="row">
+                        <label htmlFor="message">Message:</label>
+                        <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+                    </div>
+                    {errorMessage && (
+                        <div>
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                    )}
+                    <button data-testid="button" type="submit">Submit</button>
+                </form>
+            </div>
         </section>
     )
 }
