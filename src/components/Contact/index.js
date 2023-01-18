f01b3727338bcd6c7f640bce41689b0b057b0bc4
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2023-01-18 06:16:34 -0800

    changed contact form to email link

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,66 +1,72 @@
 import React, { useState } from 'react'
 
-import { validateEmail } from '../../utils/helpers'
+// import { validateEmail } from '../../utils/helpers'
 
-function ContactForm () {
-    const [formState, setFormState] = useState({ name: '', emali: '', message: '' })
+function ContactForm() {
+    //     const [formState, setFormState] = useState({ name: '', emali: '', message: '' })
 
-    const [errorMessage, setErrorMessage] = useState('')
-    const { name, email, message } = formState
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if (!errorMessage) {
-            console.log('Submit form', formState)
-            setErrorMessage('Thanks! I look forward to chatting with you.')
-        }
-    }
+    //     const [errorMessage, setErrorMessage] = useState('')
+    //     const { name, email, message } = formState
 
-    const handleChange = (e) => {
-        if (e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value)
-            if (!isValid) {
-                setErrorMessage('Please enter a valid email address')
-            } else {
-                setErrorMessage('')
-            }
-        } else {
-            if (!e.target.value.length) {
-                setErrorMessage(`Please enter ${e.target.name}.`)
-            } else {
-                setErrorMessage('')
-            }
-        }
-        if (!errorMessage) {
-            setFormState({ ...formState, [e.target.name]: e.target.value })
-            console.log('Handle form', formState)
-        }
-    }
+    //     const handleSubmit = (e) => {
+    //         e.preventDefault()
+    //         if (!errorMessage) {
+    //             console.log('Submit form', formState)
+    //             setErrorMessage('Thanks! I look forward to chatting with you.')
+    //         }
+    //     }
 
-    return(
+    // const handleChange = (e) => {
+    //     if (e.target.name === 'email') {
+    //         const isValid = validateEmail(e.target.value)
+    //         if (!isValid) {
+    //             setErrorMessage('Please enter a valid email address')
+    //         } else {
+    //             setErrorMessage('')
+    //         }
+    //     } else {
+    //         if (!e.target.value.length) {
+    //             setErrorMessage(`Please enter ${e.target.name}.`)
+    //         } else {
+    //             setErrorMessage('')
+    //         }
+    //     }
+    //     if (!errorMessage) {
+    //         setFormState({ ...formState, [e.target.name]: e.target.value })
+    //         console.log('Handle form', formState)
+    //     }
+    // }
+
+    return (
+        // <section className="container" id="contact">
+        //     <h1 data-testid="h1tag">Contact me</h1>
+        //     <div className="d-flex">
+        //         <form id="contact-form" onSubmit={handleSubmit}>
+        //             <div className="row">
+        //                 <label htmlFor="name">Name:</label>
+        //                 <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+        //             </div>
+        //             <div className="row">
+        //                 <label htmlFor="email">Email address:</label>
+        //                 <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+        //             </div>
+        //             <div className="row">
+        //                 <label htmlFor="message">Message:</label>
+        //                 <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+        //             </div>
+        //             {errorMessage && (
+        //                 <div>
+        //                     <p className="error-text">{errorMessage}</p>
+        //                 </div>
+        //             )}
+        //             <button data-testid="button" type="submit">Submit</button>
+        //         </form>
+        //     </div>
+        // </section>
         <section className="container" id="contact">
             <h1 data-testid="h1tag">Contact me</h1>
-            <div className="d-flex">
-                <form id="contact-form" onSubmit={handleSubmit}>
-                    <div className="row">
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-                    </div>
-                    <div className="row">
-                        <label htmlFor="email">Email address:</label>
-                        <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-                    </div>
-                    <div className="row">
-                        <label htmlFor="message">Message:</label>
-                        <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-                    </div>
-                    {errorMessage && (
-                        <div>
-                            <p className="error-text">{errorMessage}</p>
-                        </div>
-                    )}
-                    <button data-testid="button" type="submit">Submit</button>
-                </form>
+            <div className="d-flex align-center">
+                <p>Get in touch: <a href='mailto:albqueen@gmail.com'>albqueen@gmail.com</a>. I look forward to chatting with you.</p>
             </div>
         </section>
     )
